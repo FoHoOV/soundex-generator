e22e1e74a0b0b5decfb3a9b2918035d025f4e73a
alireza_stm <stm.stm155@gmail.com>
2023-01-08 21:22:43 +0330

    updated comments

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from soundex import html_parser, tokenizer, soundex_generator, utils
 
 
 def main(show_same_soundexes_only: bool):
-    # I count special characters such as [@, _] as normal characters that words can start with for exp: _salam is a valid token
+    # I count special characters such as [@, _] as normal characters that words can start with for exp: _what is a valid token
     # but the soundex generator filters the tokens by calling token.isalpha()
     raw_text = html_parser.get_text_from_url(html_parser.TEST_URL)
     tokens = tokenizer.extract_tokens(raw_text)
